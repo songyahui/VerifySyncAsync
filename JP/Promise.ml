4ece5b44e02c21890c20637e014019f8f20d346b
songyahui <songyahuiapp@hotmail.com>
2021-01-02 23:46:50 +0800

    m

--- JP/Promise.ml
+++ JP/Promise.ml
@@ -2,7 +2,21 @@ open String
 open List
 open Printf
 open Int32
-open Ast
+
+
+
+type ('a, 'b) promise = P | F of 'a | R of 'b
+
+type reaction = Lam | Default
+
+type ('a, 'b) promise_react = (reaction * ('a, 'b) promise ref)
+
+type ('a, 'b) fulfillReactions = (('a, 'b) promise ref * ('a, 'b) promise_react list )
+
+type ('a, 'b) rejectReactions = (('a, 'b) promise ref * ('a, 'b) promise_react list )
+
+type ('a, 'b) queue = (('a, 'b) promise * reaction * ('a, 'b) promise ref) list
+
 
 exception Foo of string
 
