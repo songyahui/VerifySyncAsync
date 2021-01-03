31ba30cc1a1f32d19e3580733570aa0116265673
songyahui <songyahuiapp@hotmail.com>
2021-01-03 11:06:46 +0800

    m

--- JP/Promise.ml
+++ JP/Promise.ml
@@ -3,21 +3,82 @@ open List
 open Printf
 open Int32
 
+exception Foo of string
 
+type dyn = Num of int | Str of string
 
-type ('a, 'b) promise = P | F of 'a | R of 'b
+type state = P | F of dyn | R of dyn
+
+type addr = int 
+
+type promise = (addr * state)
 
 type reaction = Lam | Default
 
-type ('a, 'b) promise_react = (reaction * ('a, 'b) promise ref)
+type promise_reaction = (reaction * addr)
 
-type ('a, 'b) fulfillReactions = (('a, 'b) promise ref * ('a, 'b) promise_react list )
+type fulfillReaction = (addr * promise_reaction list )
 
-type ('a, 'b) rejectReactions = (('a, 'b) promise ref * ('a, 'b) promise_react list )
+type rejectReaction = (addr * promise_reaction list )
 
-type ('a, 'b) queue = (('a, 'b) promise * reaction * ('a, 'b) promise ref) list
+type queue = (dyn * reaction * addr) list
+
+type expr = Undef 
+          | Promisify 
+          | Resolve of addr * dyn
+          | Reject of addr * dyn
+          | OnResolve of addr * reaction
+          | OnReject of addr * reaction
+          | Link of addr * addr
+
+type program_state = ((promise) list * fulfillReaction list * rejectReaction list * queue)
+
+let promise_counter = ref (-1) 
+
+let addr_generator = 
+  promise_counter := !promise_counter + 1;
+  !promise_counter
+  ;;
+
+let rec settle_promise (addr:addr) (x:dyn) (ps:promise list) (flag): promise list = 
+  let rec aux front _ps = 
+    match _ps with 
+      [] -> raise (Foo "settling an non-existing promise")
+    | (a, p) ::xs -> 
+      if a == addr then 
+      (match p with 
+      | P -> List.append front (if flag then ((a, F x) :: xs) else ((a, R x) :: xs))
+      | _-> List.append front _ps
+      )
+      else aux (List.append front [(a, p)]) xs
+  in aux [] ps
+      ;;
 
 
-exception Foo of string
+
+let forward (program_state:program_state) (expr:expr) :program_state = 
+  let (ps, f_reacts, r_reacts, q) = program_state in 
+  match expr with 
+  | Undef -> program_state 
+  | Promisify -> 
+    let (new_p:promise) = (addr_generator, P)  in 
+    ((List.append ps [(new_p)]), f_reacts, r_reacts, q)
+
+  | Resolve (a, v) -> 
+    let (new_ps:promise list) = settle_promise a v ps true in 
+    (new_ps, f_reacts, r_reacts, q)
+
+  | Reject (a, v) -> 
+    let (new_ps:promise list) = settle_promise a v ps false in 
+    (new_ps, f_reacts, r_reacts, q)
+
+  (*
+  | OnResolve of promise * reaction
+  | OnReject of promise * reaction
+  | Link of promise * promise
+  *)
+  | _ -> program_state 
+  ;;
+
 
 let () = print_string ("yahui song")
